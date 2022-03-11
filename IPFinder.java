9fe37cc71c7dbade822291d12932f3a29f5cfbbb
john632 <71838887+johnie362@users.noreply.github.com>
2022-03-11 12:25:22 +0300

    Add files via upload

---
+++ IPFinder.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.net.*;
+
+
+public class IPFinder extends JFrame implements ActionListener{
+    JLabel l;
+    JTextField tf;
+    JButton b;
+    IPFinder (){
+        super ("IP Finder");
+        l= new JLabel("Enter the Address");
+        tf= new JTextField();
+        b= new JButton("Find");
+        l.setBounds(50,70,150, 20);
+        tf.setBounds(50,100,200,20);
+        b.setBounds(50,150,80,30);
+        b.addActionListener(this);
+        add(l);
+        add(tf);
+        add(b);
+        setSize(300,300);
+        setLayout(null);
+        setVisible(true);
+    }
+public void actionPerformed (ActionEvent e)
+{
+    String url = tf.getText();
+    try {
+        InetAddress ia = InetAddress.getByName(url);
+        String ip = ia.getHostAddress();
+        JOptionPane.showMessageDialog(this, ip);
+    }
+    catch (UnknownHostException e1)
+    {
+        JOptionPane.showMessageDialog(this, e1.toString());
+
+    }
+}
+public static void main (String a[]) {
+    new IPFinder();
+}
+}
+
